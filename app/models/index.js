2d202aba4f7a696d112f9ed86c19c85df7394fff
Fang Liu <fangyiliu@gmail.com>
2020-12-09 23:08:08 -0800

    Require ssl for db connection

--- app/models/index.js
+++ app/models/index.js
@@ -6,6 +6,12 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
 
     pool: {
       max: +process.env.DB_pool_max,
