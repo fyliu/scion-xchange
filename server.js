3fc7d1045222c2be6e61ddb6eba5bf40b247f1ce
Fang Liu <fangyiliu@gmail.com>
2020-12-14 09:05:58 -0800

    Add back cors and set production conditional
    Also personalize app.

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require('path');
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
-const CLIENT_FILES = path.join(__dirname, '..', 'frontend', 'build');
+const CLIENT_FILES = path.join(__dirname, "..", "frontend", "build");
 
-app.use(function (req, res, next) {
-  res.setHeader(
-    'Content-Security-Policy-Report-Only',
-    "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'"
-  );
-  next();
-});
+app.use(cors());
+// Static file declaration for production mode
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(CLIENT_FILES));
+}
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
@@ -20,28 +19,25 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// serve client files
-app.use(express.static(CLIENT_FILES));
-
 // database
 const db = require("./app/models");
 const Role = db.role;
 
 // db.sequelize.sync();
 // force: true will drop the table if it already exists
-db.sequelize.sync({force: true}).then(() => {
-  console.log('Drop and Resync Database with { force: true }');
+db.sequelize.sync({ force: true }).then(() => {
+  console.log("Drop and Resync Database with { force: true }");
   initial();
 });
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to the scion exchange application." });
 });
 
 // routes
-require('./app/routes/auth.routes')(app);
-require('./app/routes/user.routes')(app);
+require("./app/routes/auth.routes")(app);
+require("./app/routes/user.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
@@ -52,16 +48,16 @@ app.listen(PORT, () => {
 function initial() {
   Role.create({
     id: 1,
-    name: "user"
+    name: "user",
   });
- 
+
   Role.create({
     id: 2,
-    name: "moderator"
+    name: "moderator",
   });
- 
+
   Role.create({
     id: 3,
-    name: "admin"
+    name: "admin",
   });
 }
