983961d8531c61fcea492cb84f827651101f005c
Fang Liu <fangyiliu@gmail.com>
2020-12-15 16:44:37 -0800

    Modify client to work locally

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ const App = () => {
     <div>
       <nav className="navbar navbar-expand navbar-dark bg-dark">
         <Link to={"/"} className="navbar-brand">
-          bezKoder
+          Scion Exchange
         </Link>
         <div className="navbar-nav mr-auto">
           <li className="nav-item">
