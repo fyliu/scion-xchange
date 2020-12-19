b8d94f7a277e215f0b0e9f2499f2392f283c7e44
Fang Liu <fangyiliu@gmail.com>
2020-12-18 20:49:50 -0800

    Setup sequelize

---
+++ config/config.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+
+module.exports = {
+  development: {
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  },
+  test: {
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    dialect: "postgres"
+  },
+  production: {
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    dialect: "postgres"
+  }
+};
--- seeders/20201218012339-sample-cultivars.js
+++ seeders/20201218012339-sample-cultivars.js
@@ -20,5 +20,5 @@ module.exports = {
 
   down: async (queryInterface, Sequelize) => {
     await queryInterface.bulkDelete("plants", null, {});
-  };
+  }
 };
