b320309406123fa508b7dc89fc26626c85f0c223
Fang Liu <fangyiliu@gmail.com>
2020-12-29 11:47:48 -0800

    Remove unused code

--- src/services/cultivar.service.js
+++ src/services/cultivar.service.js
@@ -33,26 +33,3 @@ export default {
   remove,
   findByName
 };
-
-//const getPublicContent = () => {
-//  return axios.get(API_URL + "all");
-//};
-//
-//const getUserBoard = () => {
-//  return axios.get(API_URL + "user", { headers: authHeader() });
-//};
-//
-//const getModeratorBoard = () => {
-//  return axios.get(API_URL + "mod", { headers: authHeader() });
-//};
-//
-//const getAdminBoard = () => {
-//  return axios.get(API_URL + "admin", { headers: authHeader() });
-//};
-//
-//export default {
-//  getPublicContent,
-//  getUserBoard,
-//  getModeratorBoard,
-//  getAdminBoard,
-//};
