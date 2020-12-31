33c99e0c9ee9008fd5f9b56350df9dea22ec8cc6
Fang Liu <fangyiliu@gmail.com>
2020-12-30 19:48:35 -0800

    Refactor trade page into view components

--- src/components/Trade.js
+++ src/components/Trade.js
@@ -1,24 +1,22 @@
 import React, { useState, useEffect } from "react";
 import UserService from "../services/user.service";
+import TradeWantByUser from "./TradeWantByUser";
+import TradeWantByCultivar from "./TradeWantByCultivar";
 
 const Trade = () => {
-  const [trades, setTrades] = useState([]);
+  const [wantCultivars, setWantCultivars] = useState([]);
   const [offers, setOffers] = useState({});
   const [message, setMessage] = useState("");
-  const [currentCultivar, setCurrentCultivar] = useState(null);
-  const [currentIndex, setCurrentIndex] = useState(-1);
-  const [currentOffer, setCurrentOffer] = useState(null);
-  const [currentOfferIndex, setCurrentOfferIndex] = useState(-1);
 
   useEffect(() => {
-    retrieveTrades();
+    retrieveWantCultivars();
   }, []);
 
-  const retrieveTrades = () => {
+  const retrieveWantCultivars = () => {
     UserService.getTradeWants()
       .then((res) => {
         //console.log(res.data);
-        setTrades(res.data);
+        setWantCultivars(res.data);
         let offers = {};
         res.data.map((cultivar) => {
           cultivar.offers.map((offer) => {
@@ -38,16 +36,6 @@ const Trade = () => {
       });
   };
 
-  const setActiveCultivar = (cultivar, index) => {
-    setCurrentCultivar(cultivar);
-    setCurrentIndex(index);
-  };
-
-  const setActiveOffer = (offer, index) => {
-    setCurrentOffer(offer);
-    setCurrentOfferIndex(index);
-  };
-
   return (
     <div className="list row">
       <div className="col-md-8 mb-3">
@@ -56,104 +44,8 @@ const Trade = () => {
         <p>{message}</p>
       </div>
 
-      <div className="col-md-6 mb-3">
-        <h4>People View</h4>
-
-        <ul className="list-group">
-          {offers &&
-            Object.entries(offers).map((offer, index) => (
-              <li
-                className={
-                  "list-group-item " +
-                  (index === currentOfferIndex ? "active" : "")
-                }
-                onClick={() => setActiveOffer(offer, index)}
-                key={index}
-              >
-                {offer[0]}
-              </li>
-            ))}
-        </ul>
-      </div>
-      <div className="col-md-6">
-        {currentOffer ? (
-          <div>
-            <h4>Availability</h4>
-            <div>
-              <label>
-                <strong>Email:</strong>
-              </label>{" "}
-              <p className="ml-3">{currentOffer[1].email}</p>
-            </div>
-            <div>
-              <label>
-                <strong>Has:</strong>
-              </label>{" "}
-              <ul className="ml-3">
-                {Array.from(currentOffer[1].cultivars).map(
-                  (cultivar, index) => (
-                    <li key={index}>{cultivar}</li>
-                  )
-                )}
-              </ul>
-            </div>
-          </div>
-        ) : (
-          <div>
-            <br />
-            <p>Please click on a user...</p>
-          </div>
-        )}
-      </div>
-      <div className="col-md-6 mb-3">
-        <h4>Cultivars View</h4>
-
-        <ul className="list-group">
-          {trades &&
-            trades.map((cultivar, index) => (
-              <li
-                className={
-                  "list-group-item " + (index === currentIndex ? "active" : "")
-                }
-                onClick={() => setActiveCultivar(cultivar, index)}
-                key={index}
-              >
-                {cultivar.name}
-              </li>
-            ))}
-        </ul>
-      </div>
-      <div className="col-md-6">
-        {currentCultivar ? (
-          <div>
-            <h4>Availability</h4>
-            <div>
-              <label>
-                <strong>Cultivar:</strong>
-              </label>{" "}
-              <p className="ml-3">{currentCultivar.name}</p>
-            </div>
-            <div>
-              <label>
-                <strong>From:</strong>
-              </label>{" "}
-              <dl className="ml-3">
-                {currentCultivar.offers.map((user) => (
-                  <>
-                    <dt>{user.username}</dt>
-                    <dd>{user.email}</dd>
-                  </>
-                ))}
-              </dl>
-            </div>
-          </div>
-        ) : (
-          <div>
-            <br />
-            <p>Please click on a cultivar...</p>
-          </div>
-        )}
-      </div>
+      <TradeWantByUser users={offers} />
+      <TradeWantByCultivar cultivars={wantCultivars} />
     </div>
   );
 };
---
+++ src/components/TradeWantByCultivar.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+
+const TradeWantByCultivar = ({ cultivars }) => {
+  const [currentCultivar, setCurrentCultivar] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(-1);
+
+  const setActiveCultivar = (cultivar, index) => {
+    setCurrentCultivar(cultivar);
+    setCurrentIndex(index);
+  };
+
+  return (
+    <>
+      <div className="col-md-6 mb-3">
+        <h4>Cultivars View</h4>
+
+        <ul className="list-group">
+          {cultivars &&
+            cultivars.map((cultivar, index) => (
+              <li
+                className={
+                  "list-group-item " + (index === currentIndex ? "active" : "")
+                }
+                onClick={() => setActiveCultivar(cultivar, index)}
+                key={index}
+              >
+                {cultivar.name}
+              </li>
+            ))}
+        </ul>
+      </div>
+      <div className="col-md-6">
+        {currentCultivar ? (
+          <div>
+            <h4>Availability</h4>
+            <div>
+              <label>
+                <strong>Cultivar:</strong>
+              </label>{" "}
+              <p className="ml-3">{currentCultivar.name}</p>
+            </div>
+            <div>
+              <label>
+                <strong>From:</strong>
+              </label>{" "}
+              <dl className="ml-3">
+                {currentCultivar.offers.map((user) => (
+                  <>
+                    <dt>{user.username}</dt>
+                    <dd>{user.email}</dd>
+                  </>
+                ))}
+              </dl>
+            </div>
+          </div>
+        ) : (
+          <div>
+            <br />
+            <p>Please click on a cultivar...</p>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default TradeWantByCultivar;
