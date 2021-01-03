783c71ac9f4c9e8a6645eb234ce5426cb64df225
Fang Liu <fangyiliu@gmail.com>
2021-01-03 01:02:50 -0800

    Add offer description

--- src/components/Offer.js
+++ src/components/Offer.js
@@ -34,10 +34,18 @@ const Offer = () => {
   };
 
   const handleInputChange = (e) => {
-    const id = e.target.value;
-    const value = e.target.checked;
-
-    setOffers({ ...offers, [id]: value });
+    const name = e.target.id;
+    const id = e.target.name;
+    const value =
+      e.target.type === "checkbox" ? e.target.checked : e.target.value;
+    if (name === "offerDescription") {
+      setOffers({
+        ...offers,
+        [id]: { ...offers[id], offerDescription: value }
+      });
+    } else {
+      setOffers({ ...offers, [id]: { ...offers[id], offer: value } });
+    }
   };
 
   const updateOffer = () => {
@@ -58,16 +66,44 @@ const Offer = () => {
         <form>
           {cultivars &&
             cultivars.map((cultivar) => (
-              <label key={cultivar.id}>
-                <input
-                  type="checkbox"
-                  name={cultivar.id + " " + cultivar.name}
-                  value={cultivar.id}
-                  checked={offers[cultivar.id] || false}
-                  onChange={handleInputChange}
-                />
-                {cultivar.name + " - " + cultivar.category}
-              </label>
+              <React.Fragment key={cultivar.id}>
+                <div className="form-group">
+                  <label htmlFor={cultivar.id}>
+                    <input
+                      type="checkbox"
+                      id={cultivar.id}
+                      name={cultivar.id}
+                      value={cultivar.id}
+                      checked={
+                        (offers[cultivar.id] && offers[cultivar.id].offer) ||
+                        false
+                      }
+                      onChange={handleInputChange}
+                    />
+                    {cultivar.name + " - " + cultivar.category}
+                  </label>
+                </div>
+                {offers[cultivar.id] && offers[cultivar.id].offer ? (
+                  <div className="form-group">
+                    <label htmlFor="offerDescription">
+                      Description:
+                      <textarea
+                        className="form-control"
+                        id="offerDescription"
+                        name={cultivar.id}
+                        rows="1"
+                        onChange={handleInputChange}
+                        value={
+                          offers[cultivar.id] &&
+                          offers[cultivar.id].offerDescription
+                        }
+                      />
+                    </label>
+                  </div>
+                ) : (
+                  ""
+                )}
+              </React.Fragment>
             ))}
         </form>
         <button type="submit" onClick={() => updateOffer()}>
