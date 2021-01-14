981949112c0ef6f3977d95e6516017f076a61fa5
Fang Liu <fangyiliu@gmail.com>
2021-01-14 00:27:40 -0800

    Make cultivar and category routes protected

--- frontend/src/services/category.service.js
+++ frontend/src/services/category.service.js
@@ -1,28 +1,28 @@
 import http from "../http-common";
-//import authHeader from "./auth-header";
+import authHeader from "./auth-header";
 
 const getAll = () => {
-  return http.get("/categories");
+  return http.get("/categories", { headers: authHeader() });
 };
 
 const get = (id) => {
-  return http.get(`/categories/${id}`);
+  return http.get(`/categories/${id}`, { headers: authHeader() });
 };
 
 const create = (data) => {
-  return http.post("/categories", data);
+  return http.post("/categories", data, { headers: authHeader() });
 };
 
 const update = (id, data) => {
-  return http.put(`/categories/${id}`, data);
+  return http.put(`/categories/${id}`, data, { headers: authHeader() });
 };
 
 const remove = (id) => {
-  return http.delete(`/categories/${id}`);
+  return http.delete(`/categories/${id}`, { headers: authHeader() });
 };
 
 const findByName = (name) => {
-  return http.get(`/categories?name=${name}`);
+  return http.get(`/categories?name=${name}`, { headers: authHeader() });
 };
 
 export default {
--- frontend/src/services/cultivar.service.js
+++ frontend/src/services/cultivar.service.js
@@ -1,28 +1,28 @@
 import http from "../http-common";
-//import authHeader from "./auth-header";
+import authHeader from "./auth-header";
 
 const getAll = () => {
-  return http.get("/cultivars");
+  return http.get("/cultivars", { headers: authHeader() });
 };
 
 const get = (id) => {
-  return http.get(`/cultivars/${id}`);
+  return http.get(`/cultivars/${id}`, { headers: authHeader() });
 };
 
 const create = (data) => {
-  return http.post("/cultivars", data);
+  return http.post("/cultivars", data, { headers: authHeader() });
 };
 
 const update = (id, data) => {
-  return http.put(`/cultivars/${id}`, data);
+  return http.put(`/cultivars/${id}`, data, { headers: authHeader() });
 };
 
 const remove = (id) => {
-  return http.delete(`/cultivars/${id}`);
+  return http.delete(`/cultivars/${id}`, { headers: authHeader() });
 };
 
 const findByName = (name) => {
-  return http.get(`/cultivars?name=${name}`);
+  return http.get(`/cultivars?name=${name}`, { headers: authHeader() });
 };
 
 export default {
