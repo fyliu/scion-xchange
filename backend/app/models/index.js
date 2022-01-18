86ec8475b3ffc6c198bb446b389a52197d503060
Fang Liu <fangyiliu@gmail.com>
2022-01-17 18:53:23 -0800

    Use DATABASE_URL for credentials

--- backend/app/models/index.js
+++ backend/app/models/index.js
@@ -1,31 +1,23 @@
 const config = require("../config/auth.config");
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    },
-
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    },
-    define: {
-      timestamps: false
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
     }
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+  define: {
+    timestamps: false
   }
-);
+});
 
 const db = {};
 
